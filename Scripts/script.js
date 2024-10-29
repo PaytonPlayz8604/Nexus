d1aa292b45c12852159d1a00115dd7624f8ad837
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2024-10-28 20:56:42 -0400

    Create script.js

---
+++ Scripts/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let includeHTML = document.querySelectorAll('[w3-include-html]');
+    includeHTML.forEach(function (el) {
+        let file = el.getAttribute('w3-include-html');
+        fetch(file).then(response => {
+            return response.text()
+        }).then(data => {
+            el.innerHTML = data;
+        });
+    });
+});
