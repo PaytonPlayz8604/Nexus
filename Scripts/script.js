fa08b78e059c4b20c4ff2efa95cb79048b0c9590
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2024-10-30 08:15:35 -0400

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -24,7 +24,26 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+/* Slideshow on index.html */
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+    let slides = document.querySelectorAll(".mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+/* Property Console Log */
 console.log(
     `%c
     ╔═════════════════════════════════════════════════════════╗
