a136c93a83227c2140295cc8f59ce874256fa0ca
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2024-10-30 08:23:00 -0400

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -25,22 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 /* Slideshow on index.html */
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.querySelectorAll(".mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex - 1].style.display = "block";  
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
+let slideIndex = 1;
+showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll(".mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
 
 /* Property Console Log */
