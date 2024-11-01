47603b9c112828084b03b6e6328d03137b744a43
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2024-10-31 20:45:26 -0400

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -38,3 +38,29 @@ console.log(
     `,
     'color: #00aaff; font-weight: bold;'
 );
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slide');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = 'block';
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function plusSlides(n) {
+    let slides = document.querySelectorAll('.slide');
+    slideIndex += n;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex-1].style.display = 'block';
+}
+
