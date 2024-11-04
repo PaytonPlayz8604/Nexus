d546c5e11ddd7bacfb0bdf419210d35c150b4487
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2024-11-04 08:37:48 -0500

    Update productsSold.js

--- Scripts/productsSold.js
+++ Scripts/productsSold.js
@@ -3,17 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentCount = parseInt(localStorage.getItem('productsSold')) || 1;
 
     const targetCount = Math.floor(Math.random() * 10000) + 1;
-    const increment = Math.ceil((targetCount - currentCount) / 100); // Adjust the increment for a smooth count up
-    const intervalTime = 100; // Adjust the interval time for desired speed
+    const increment = Math.ceil((targetCount - currentCount) / 200); 
+    const intervalTime = 500; 
+
+    function formatNumber(num) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
     function updateProductsSold() {
         if (currentCount < targetCount) {
             currentCount += increment;
-            productsSoldElement.textContent = currentCount;
+            productsSoldElement.textContent = formatNumber(currentCount);
             localStorage.setItem('productsSold', currentCount);
             setTimeout(updateProductsSold, intervalTime);
         } else {
-            productsSoldElement.textContent = targetCount;
+            productsSoldElement.textContent = formatNumber(targetCount);
             localStorage.setItem('productsSold', targetCount);
         }
     }
